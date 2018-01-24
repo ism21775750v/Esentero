5bab23556461691505b5ffe059fc3a15d62893d1
ism21775750 <ism21775750@a01.informatica.escoladeltreball.org>
2018-01-24 12:15:58 +0100

    first commit

---
+++ divisores.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+divisor=input("Introduzca un divisor:")
+dividendo=input("Introduzca un dividendo:")
+if divisor%dividendo ==0:
+	print "Correcto"
+else:
+	print "No es correcto"
+
+# Esentero
